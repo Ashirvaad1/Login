ff509cdc169c2278e6072db9e639c7124f780463
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-08-04 01:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let emails=["gargi68757@gmail.com", "laxman67884@gmail.com", "roshni55758@gmail.com", "sanjana58578@gmail.com", "uma685785@gmail.com", "zulfiqar66476@gmail.com"];
+let pos=0;
+document.getElementById('copy').addEventListener('click', function()
+{
+	const index=document.getElementById('index');
+	index.value=(pos+2);
+	const ta=document.getElementById('ta');
+	ta.select();
+	document.execCommand('copy');
+	pos++;
+	if(pos===6)
+	{
+		pos=0;
+		index.value=(pos+1);
+	}
+	ta.value=emails[pos];
+});
