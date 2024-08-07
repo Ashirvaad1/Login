4da06f5c92e81a1731cfe1acf435cd0a434e4c54
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-08-08 00:21:31 +0530

    script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let passwords=["{ Gargi@1}", "{ Laxman@1}", "{ Roshni@1}", "{ Sanjana@1}", "{ Um
 let pos=0;
 document.getElementById('copy').addEventListener('click', function()
 {
+	navigator.clipboard.writeText(emails[pos]);
 	navigator.clipboard.writeText(passwords[pos]);
 	pos=(pos+1)%emails.length;
 	document.getElementById('index').value=pos+1;
