f0dd459655f0d56796b154031a126fbdf5ff2421
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-08-22 21:31:39 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let emails=["gargi68757@gmail.com", "laxman67884@gmail.com", "roshni55758@gmail.com", "sanjana58578@gmail.com", "uma685785@gmail.com", "zulfiqar66476@gmail.com", "rajesh728829@gmail.com", "uma738393@gmail.com", "abhi828886@gmail.com", "aditya010328@gmail.com", "advika292008@gmail.com", "akshat17112006@gmail.com", "aashi1382000@gmail.com", "dhruv46674@gmail.com", "shishir85842@gmail.com", "suyash55573@gmail.com"];
-let passwords=["{ Gargi@1}", "{ Laxman@1}", "{ Roshni@1}", "{ Sanjana@1}", "{ Uma@1}", "{ Zulfiqar@1}", "{ Rajesh@1}", "{ Uma@1}", "{ Abhi@1}", "{ Aditya@1}", "{ Advika@1}", "{ Akshat@1}", "{ Aashi@1}", "{ Dhruv@1}", "{ Shishir@1}", "{ Suyash@1}"];
-let pos=0;
+let emails=["gargi68757@gmail.com", "laxman67884@gmail.com", "roshni55758@gmail.com", "sanjana58578@gmail.com", "uma685785@gmail.com", "zulfiqar66476@gmail.com", "rajesh728829@gmail.com", "uma738393@gmail.com", "abhi828886@gmail.com", "aditya010328@gmail.com", "advika292008@gmail.com", "akshat17112006@gmail.com", "aashi1382000@gmail.com", "dhruv46674@gmail.com", "shishir85842@gmail.com", "suyash55573@gmail.com", "vijay8575337@gmail.com", "sooraj82892@gmail.com", "watson82827@gmail.com"];
+let passwords=["{ Gargi@1}", "{ Laxman@1}", "{ Roshni@1}", "{ Sanjana@1}", "{ Uma@1}", "{ Zulfiqar@1}", "{ Rajesh@1}", "{ Uma@1}", "{ Abhi@1}", "{ Aditya@1}", "{ Advika@1}", "{ Akshat@1}", "{ Aashi@1}", "{ Dhruv@1}", "{ Shishir@1}", "{ Suyash@1}", "{ Vijay@1}", "{ Sooraj@1}", "{ Watson@1}"];
+let pos;
 let check=1;
+window.onload=function()
+{
+	if(localStorage.getItem('pos')!=null)
+		pos=parseInt(localStorage.getItem('pos'), 10);
+	else
+		pos=0;
+	document.getElementById('index').value=pos+1;
+	document.getElementById('email').value=emails[pos];
+}
 document.getElementById('copy').addEventListener('click', function()
 {
 	if(check===1)
@@ -14,6 +23,7 @@ document.getElementById('copy').addEventListener('click', function()
 		navigator.clipboard.writeText(passwords[pos]);
 		check=1;
 		pos=(pos+1)%emails.length;
+		localStorage.setItem('pos', pos);
 		document.getElementById('index').value=pos+1;
 		document.getElementById('email').value=emails[pos];
 	}
