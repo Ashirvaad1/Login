69f92bacffb1cd4b159f038cfb6fef8388a81466
Ashirvaad Verma <ashirvaadverma2000@zohomail.in>
2024-11-02 13:53:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let emails=["laxman67884@gmail.com", "roshni55758@gmail.com", "sanjana58578@gmail.com", "uma685785@gmail.com", "zulfiqar66476@gmail.com", "rajesh728829@gmail.com", "uma738393@gmail.com"];
-let passwords=["{ Laxman@1}", "{ Roshni@1}", "{ Sanjana@1}", "{ Uma@1}", "{ Zulfiqar@1}", "{ Rajesh@1}", "{ Uma@1}"];
+let emails=["akritimishra336@gmail.com", "ankitagond290@gmail.com", "ankitchaurasia8282@gmail.com", "chaturdwivedi678@gmail.com", "diyabaati07@gmail.com", "durgeshdubey4519@gmail.com", "jacobthomas9808@gmail.com", "jayasaini932@gmail.com", "kalluseth127@gmail.com", "krishnakashyap6820@gmail.com", "laxman67884@gmail.com", "princeverma4736@gmail.com", "rajesh728829@gmail.com", "roshni55758@gmail.com", "samarpandey964@gmail.com", "sanjana58578@gmail.com", "uma685785@gmail.com", "uma738393@gmail.com", "vandanakumari28921@gmail.com", "yashchaurasia533@gmail.com", "yashipatel127@gmail.com", "zulfiqar66476@gmail.com"];
+let passwords=["{ Akriti@1}", "{ Ankita@1}", "{ Ankit@1}", "{ Chatur@1}", "{ Diya@1}", "{ Durgesh@1}", "{ Jacob@1}", "{ Jaya@1}", "{ Kallu@1}", "{ Krishna@1}", "{ Laxman@1}", "{ Prince@1}", "{ Rajesh@1}", "{ Roshni@1}", "{ Samar@1}", "{ Sanjana@1}", "{ Uma@1}", "{ Uma@1}", "{ Vandana@1}", "{ Yash@1}", "{ Yashi@1}", "{ Zulfiqar@1}"];
 let pos;
 let check=1;
 window.onload=function()
